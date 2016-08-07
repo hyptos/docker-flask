fe4ecf37964e78631804bd4141fbb0c4c4c19513
Antoine Martin <hyptos974@gmail.com>
2016-08-07 19:35:15 +0200

    recuperation des params post ok

--- web/app.py
+++ web/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+import smtplib
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,6 +7,16 @@ app = Flask(__name__)
 @app.route('/index')
 def home():
     return render_template('index.html', title='Home')
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    strMessage 	= request.form['message']
+    strName 	= request.form['name']
+    strEmail 	= request.form['email']   
+    s 		= smtplib.SMTP('localhost')
+    s.sendmail("hyptos974@gmail.com", strEmail, strMessage)
+    s.quit()
+    return "Your message has been sent !"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
