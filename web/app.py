d16bf032b44ee2918b42279cbbbba3e8044141ee
Antoine Martin <hyptos974@gmail.com>
2016-08-07 08:05:03 +0200

    added static ressources for my website

--- web/app.py
+++ web/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello from Docker'
+@app.route('/home')
+@app.route('/index')
+def home():
+    return render_template('index.html', title='Home')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
